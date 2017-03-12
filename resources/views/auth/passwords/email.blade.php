4e6060009e9b644438aaaeabf8662946cd56b5ab
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2017-03-12 20:39:56 +0100

    Redesigned the reset-email page.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.game')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    @include('parts.resetemail')
+@endsection
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+@section('extra-js')
+    <script>
+        $(document).ready(function() {
+            $('#resetemail_window').dialog({
+                width: 300,
+                height: 260,
+                autoOpen : true,
+                resizable: false,
+                closeOnEscape: false,
+                open: function(){
+                    $(".ui-dialog-titlebar-close").hide();
+                    $(".ui-dialog-titlebar-min").hide();
+                },
+                position: {my: "center", at: "center", of: $("body"),within: $("body") }
+            });
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+            $(".ui-dialog-titlebar-min").hide();
+        });
+    </script>
 @endsection
---
+++ resources/views/parts/resetemail.blade.php
@@ -0,0 +1,35 @@
+<div id="resetemail_window" class="dialog_window" title="Reset password">
+    <div class="panel-body">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form role="form" method="POST" action="{{ route('password.email') }}">
+            {{ csrf_field() }}
+
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div>
+                    <button type="submit" class="btn btn-primary">
+                        Send Password Reset Link
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
