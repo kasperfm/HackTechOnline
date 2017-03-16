a9549c8f09265e3492a84222ab32a34ad5952ff1
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2017-03-16 20:19:17 +0100

    Changed error messages in resetemail blade file.

--- resources/views/parts/resetemail.blade.php
+++ resources/views/parts/resetemail.blade.php
@@ -1,25 +1,17 @@
 <div id="resetemail_window" class="dialog_window" title="Reset password">
     <div class="panel-body">
         @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
+            <script>alert("{{ session('status') }}");</script>
         @endif
 
         <form role="form" method="POST" action="{{ route('password.email') }}">
             {{ csrf_field() }}
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <div class="form-group">
                 <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                 <div class="col-md-6">
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' has-input-error' : '' }}" name="email" value="{{ old('email') }}" required>
                 </div>
             </div>
 
