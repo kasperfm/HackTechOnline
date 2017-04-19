9e18d1e92fb6889efbf6700e4ce436ae13dc2dc8
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2017-04-19 19:03:48 +0200

    Added bank migrations and basic models.

---
+++ database/migrations/2017_04_19_183311_create_bank_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bank_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('account_number')->unsigned();
+            $table->tinyInteger('active')->default(1);
+            $table->bigInteger('balance')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
---
+++ database/migrations/2017_04_19_184835_create_bank_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBankTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('to_bank_id');
+            $table->integer('to_account');
+            $table->integer('from_bank_id')->nullable();
+            $table->integer('from_account')->nullable();
+            $table->bigInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_transactions');
+    }
+}
