a46c47eb92719e408638ac28d962e4045f51bc89
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2020-04-09 19:42:04 +0200

    Fixed a deprecated function in the StringHelper.

--- app/Classes/Helpers/StringHelper.php
+++ app/Classes/Helpers/StringHelper.php
@@ -8,6 +8,6 @@ class StringHelper
     public static function camelCaseToWords($str) {
         $splitCamelArray = preg_split('/(?=[A-Z])/', $str);
 
-        return ucwords(implode($splitCamelArray, ' '));
+        return ucwords(implode(' ', $splitCamelArray,));
     }
 }
