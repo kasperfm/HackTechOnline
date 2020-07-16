d5121e58c5ae987f8448a330224a409cafc6ab61
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2020-07-16 23:15:51 +0200

    Changed a few things in the backpack config.

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -105,10 +105,10 @@ return [
     // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'Kasper Færch Mikkelsen',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://kasperfm.com',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => false,
