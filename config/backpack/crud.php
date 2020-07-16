40281c6fe5e9cc3ceebfdf690fb3e9e91a86581b
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2020-07-16 23:34:47 +0200

    More config changes.

--- config/backpack/crud.php
+++ config/backpack/crud.php
@@ -162,7 +162,7 @@ return [
     |-------------------
     */
 
-    'show_translatable_field_icon'     => true,
+    'show_translatable_field_icon'     => false,
     'translatable_field_icon_position' => 'right', // left or right
 
     'locales' => [
